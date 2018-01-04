8f078bfaa9915fc709dba2deb3bbb27a05e6677f
Antonio Luis <luisinweb@gmail.com>
2018-01-04 11:01:45 -0300

    corrige erros de padronização de código

---
+++ src/main.js
@@ -0,0 +1,11 @@
+const foo = 'some';
+const a = foo;
+const b = 2;
+// const b = 4;
+const c = 'açldkfjsldkfjsçlkfjsdlkfjlaksdk34j3lkrjs0r9j33jrkWL3K4J3LKJçwl43909fsdjlw3k4jwer9w0j3l4kj3l4kj3lk4jw3lkjsdf0s90';
+const result = foo + a + b + c;
+function someFunc() {
+  return result;
+}
+
+someFunc();
